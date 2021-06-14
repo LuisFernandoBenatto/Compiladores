8a7ba855952a6c6836b64ad86f916b273b3e50cf
Luís Fernando Benatto <luisbenatto5@gmail.com>
2021-06-14 18:00:10 -0300

    Simple Translator em C#

---
+++ SimpleTranslator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SimpleTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lexer = new SimpleLexer("225 + 968   - 78879    + 1564564");
+            var parser = new SimpleParser(lexer);
+            parser.Expr();
+            Console.WriteLine(parser.Output);
+            //TestLexer(lexer);
+        }
+
+        private static void TestLexer(SimpleLexer lexer)
+        {
+            while (lexer.HasInput)
+            {
+                var token = lexer.NextToken();
+                Console.WriteLine("<"+token.Type+ (token.HasValue?","+token.Value:"")+">");
+            }
+        }
+    }
+}
