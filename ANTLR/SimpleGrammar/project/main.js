78b32bddb1d75df3ab468703f3eb58118090c4c7
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2021-11-22 23:26:31 -0300

    Parser Generator

--- ANTLR/SimpleGrammar/project/main.js
+++ ANTLR/SimpleGrammar/project/main.js
@@ -4,6 +4,29 @@ import SimpleGrammarParser from './Grammar/SimpleGrammarParser.js';
 import SimpleGrammarListener from './Grammar/SimpleGrammarListener.js';
 
 // const input = fetch('input.txt').then(response => response.text());
+
+class MyErrorListener{
+    constructor(ErrorListener){
+        this.ErrorListener = ErrorListener;
+    }
+    syntaxError(offendingSymbol, line, column, e){
+        console.log(`ERRO!!! O erro está na linha ${line}:${column}. Simbolo não apropriado: ${offendingSymbol.text}`);
+    }
+}
+
+class MyGrammarListener {
+    constructor (SimpleGrammarListener) {
+        this.SimpleGrammarListener = SimpleGrammarListener;
+    }
+    enterFact(ctx = SimpleGrammarParser.FactContext){
+        console.log('In fact');
+
+    }
+    exitFact(ctx = SimpleGrammarParser.FactContext){
+        console.log('Out fact')
+    }
+}
+
 const input = "22+27;";
 const chars = new antlr4.InputStream(input);
 const lexer = new SimpleGrammarLexer(chars);
